83f43bf93a5d61fd6d09cb707c8c71c9819e3226
RPICoder <techcoder20@gmail.com>
2021-03-11 07:01:53 +0000

    Added help command

--- main.py
+++ main.py
@@ -90,6 +90,66 @@ async def ban(ctx, member : discord.Member,*, reason=None):
   await member.ban(reason=reason)
   await ctx.send(f'{member} has been banned from the server. Reason for ban is {reason}')
 
+client.remove_command("help")
+
+@client.group(invoke_without_command=True)
+async def help(ctx):
+  em = discord.Embed(title = 'Help', description='The help argument will show you the available arguments. Use `rpi <argument name>` to get more information on a command.\n\n **The Available Arguments are:**', color = discord.Color.green())
+
+  em.add_field(name = 'Moderation', value='kick, ban, clear')
+  em.add_field(name = 'Helping Others', value = 'exec, search')
+  em.add_field(name = 'Other', value = 'ping')
+  em.set_thumbnail(url = 'https://publicdomainpictures.net/pictures/150000/velka/help-icon.jpg')
+  em.set_footer(icon_url = ctx.author.avatar_url, text = f'Requested by {ctx.author.name}')
+  await ctx.send(embed = em)
+
+@help.command(name='kick')
+async def _kick(ctx):
+  em = discord.Embed(title = 'Kick', description = 'Kick any member in your server. The reason by default is set to none. The user need Administrator permission to use this argument.')
+  em.add_field(name='Usage', value='rpi kick [Member Name] [Reason]')
+  em.set_footer(icon_url = ctx.author.avatar_url, text = f'Requested by {ctx.author.name}')
+  await ctx.send(embed = em)
+
+
+@help.command(name='ban')
+async def _ban(ctx):
+  em = discord.Embed(title = 'Ban', description = 'Ban any member in your server. The reason by default is set to none. The user need Administrator permission to use this argument.')
+  em.add_field(name='Usage', value='rpi ban [Member Name] [Reason]')
+  em.set_footer(icon_url = ctx.author.avatar_url, text = f'Requested by {ctx.author.name}')
+  await ctx.send(embed = em)
+
+
+@help.command(name='clear')
+async def _clear(ctx):
+  em = discord.Embed(title = 'Clear', description = 'Delete a specific number of messages. It takes one argument which is Number of messages. THis command will only work if the user has manage messages permission.')
+  em.add_field(name='Usage', value='rpi clear [Number of messages]')
+  em.set_footer(icon_url = ctx.author.avatar_url, text = f'Requested by {ctx.author.name}')
+  await ctx.send(embed = em)
+
+
+@help.command(name='search')
+async def __search(ctx):
+  em = discord.Embed(title = 'Search', description = 'This argument will help you to search google right from discord. It takes two arguments which are Number of search results and Search query.')
+  em.add_field(name='Usage', value='rpi search [Number of Search Results] [Search query]')
+  em.set_footer(icon_url = ctx.author.avatar_url, text = f'Requested by {ctx.author.name}')
+  await ctx.send(embed = em)
+
+
+@help.command(name='exec')
+async def _exec(ctx):
+  em = discord.Embed(title = 'Exec', description = 'This argument will allow you to run commands in a linux terminal and see the output of the command.')
+  em.add_field(name='Usage', value='rpi exec [command]')
+  em.set_footer(icon_url = ctx.author.avatar_url, text = f'Requested by {ctx.author.name}')
+  await ctx.send(embed = em)
+
+
+@help.command(name='ping')
+async def _ping(ctx):
+  em = discord.Embed(title = 'Ping', description = 'Sends the latency of the bot.')
+  em.add_field(name='Usage', value='rpi ping')
+  em.set_footer(icon_url = ctx.author.avatar_url, text = f'Requested by {ctx.author.name}')
+  await ctx.send(embed = em)
+
 
 @client.command()
 async def test(ctx):
